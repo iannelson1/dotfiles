e3d1b90505aa7b3a59072189fda716097fda5f67
Ian Nelson <iannelson1@gmail.com>
2013-11-27 13:59:34 -0500

    Updated Linux init script.

--- init-linux.sh
+++ init-linux.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 ## Copy config files
 cp .bashrc ~/
-cp .git-completion ~/
-cp .git-prompt ~/
+cp .git-completion.bash ~/
+cp .git-prompt.sh ~/
 cp .gitconfig ~/
 cp .vimrc ~/
-
-## SSH Settings
-mkdir ~/.ssh
-cp .ssh/id_rsa ~/.ssh/id_rsa
-cp .ssh/id_rsa.pub ~/.ssh/id_rsa.pub
-cp .ssh/authorized_keys ~/.ssh/authorized_keys
-chmod 0600 ~/.ssh/id_rsa*
-chmod 0600 ~/.ssh/authorized_keys
