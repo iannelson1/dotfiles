d195d7dd2bcc2a0478431efe5cc2e97ba53ca6ad
Ian Nelson <iannelson1@gmail.com>
2016-06-25 08:24:05 -0400

    Added install for tmux themes by gpakosz

--- install.sh
+++ install.sh
@@ -10,3 +10,8 @@ curl -o - https://raw.github.com/vgod/vimrc/master/auto-install.sh | sh
 
 ## Install liquidprompt
 git clone https://github.com/nojhan/liquidprompt.git ~/.liquidprompt
+
+## Install tmux theme
+git clone https://github.com/gpakosz/.tmux.git ~/.tmux
+ln -s ~/.tmux/.tmux.conf ~/.tmux.conf
+cp .tmux.conf.local ~/.tmux.conf.local
